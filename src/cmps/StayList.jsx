f78908bc771607ecf7898c607e91b50a978c7290
Tal Elias <talelias2@gmail.com>
2023-10-03 23:17:15 +0300

    implemented list cmp

--- src/cmps/StayList.jsx
+++ src/cmps/StayList.jsx
@@ -1,6 +1,15 @@
-export function StayList() {
+import { StayPreview } from "./StayPreview";
+
+export function StayList({ stays }) {
     return (
         <section className="stay-list">
+            <ul className="clean-list">
+                {stays.map(stay =>
+                    <li key={stay._id}>
+                        <StayPreview stay={stay} />
+                    </li>
+                )}
+            </ul>
         </section>
     )
 }
