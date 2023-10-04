eb4a26253eef23582ec56c8362755f9d035fcd8e
Omer <omernitoker@gmail.com>
2023-10-04 22:45:17 +0300

    improve StayReservation cmp

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -24,7 +24,7 @@ export function RootCmp() {
                     {/* <Route path="user/:id" element={<UserDetails />} /> */}
                 </Routes>
             </main>
-            <AppFooter />
+            {/* <AppFooter /> */}
         </div>
     )
 }
