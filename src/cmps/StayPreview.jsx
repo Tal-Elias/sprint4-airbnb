9457d15b4b46d756df2804649b406ec955a2fb56
klilahkenazi <klilas2483@gmail.com>
2023-10-04 11:17:22 +0300

    stay preview updates

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,7 +1,7 @@
 export function StayPreview({stay}) {
     return (
         <article className="stay-preview">
-            <img src='https://www.brisbaneuniquehomes.com.au/wp-content/uploads/2018/04/builders-gold-coast-1-1-1024x683-1024x683.jpg'/>
+            <img className="stay-img" src='https://www.brisbaneuniquehomes.com.au/wp-content/uploads/2018/04/builders-gold-coast-1-1-1024x683-1024x683.jpg'/>
             <div className="stay-info">
             <div className="stay-loc">{stay.loc.city}, {stay.loc.country}</div>
             <span className="stay-rate">{4.55}</span>
