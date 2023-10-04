d7a734fabd38c8099012c0934ef264c53a8f74c1
Tal Elias <talelias2@gmail.com>
2023-10-04 20:02:49 +0300

    added search bar form

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -7,6 +7,7 @@ import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
 import { StayDetails } from './pages/StayDetails'
 import { StayBook } from './pages/StayBook'
+import { StaySearchBar } from './cmps/StaySearchBar'
 // import { UserDetails } from './pages/UserDetails'
 
 export function RootCmp() {
@@ -15,6 +16,7 @@ export function RootCmp() {
         <div className="main-layout">
             <AppHeader />
             <main>
+                <StaySearchBar />
                 <Routes>
                     {routes.map(route => <Route key={route.path} exact={true} element={route.component} path={route.path} />)}
                     <Route path="stay/:stayId" element={<StayDetails />} />
