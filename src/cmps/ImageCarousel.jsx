a20d609b910ef1f45433e71b2c988183897003e1
klilahkenazi <klilas2483@gmail.com>
2023-10-06 16:46:41 +0300

    more progress on stay preview

--- src/cmps/ImageCarousel.jsx
+++ src/cmps/ImageCarousel.jsx
@@ -1,51 +1,84 @@
-import React, { Component } from "react";
+import React, { Component, useRef } from "react";
 import Slider from "react-slick";
+import {IoIosArrowForward} from 'react-icons/io'
+import {IoIosArrowBack} from 'react-icons/io'
 
-function SampleNextArrow(props) {
+// function SampleNextArrow(props) {
+//     const { className, style, onClick } = props;
+//     console.log(className)
+//     return (
+//         <div
+//             className={className}
+//             style={{ ...style, display: "block"}}
+//             onClick={onClick}
+//         />
+//     );
+// }
+
+// function SamplePrevArrow(props) {
+//     const { className, style, onClick } = props;
+//     return (
+//         <div
+//             className={className}
+//             style={{ ...style, display: "inline", zIndex: 10,  }}
+//             onClick={onClick}
+//         />
+//     );
+// }
+
+function NextArrow(props){
     const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block", marginInlineEnd: '30px' }}
-            onClick={onClick}
-        />
-    );
-}
 
-function SamplePrevArrow(props) {
+    return(
+        <div className="new-button next" onClick={onClick}><IoIosArrowForward/></div>
+    )
+}
+function PrevArrow(props){
     const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "inline", zIndex: 10,  marginInlineStart: '30px' }}
-            onClick={onClick}
-        />
-    );
-}
 
+    return(
+        <div className="new-button prev" onClick={onClick}><IoIosArrowBack/></div>
+    )
+}
 export function ImageCarousel({ imgs }) {
+    const sliderRef = useRef(null);
     // render() {
     const settings = {
         dots: true,
-        infinite: false,
+        infinite: true,
+        speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />,
-        appendDots: dots => (
-            <div
-                style={{
-                    // backgroundColor: "#ddd",
-                    borderRadius: "10px",
-                    padding: "10px",
-                }}
-            >
-                <ul style={{ marginBlockEnd: "20px", color: "white" }}> {dots} </ul>
-            </div>
-        )
+        nextArrow: <NextArrow/>,
+        prevArrow: <PrevArrow/>
+
+        // nextArrow: <SampleNextArrow />,
+        // prevArrow: <SamplePrevArrow />,
+        // appendDots: dots => (
+        //     <div
+        //         style={{
+        //             // backgroundColor: "#ddd",
+        //             borderRadius: "10px",
+        //             padding: "10px",
+        //         }}
+        //     >
+        //         <ul style={{ marginBlockEnd: "20px", color: "white" }}> {dots} </ul>
+        //     </div>
+        // )
     };
+
+    const handleNextClick = () => {
+        sliderRef.current.slickNext(); // Navigate to the next slide
+      };
+    
+      const handlePrevClick = () => {
+        sliderRef.current.slickPrev(); // Navigate to the previous slide
+      };
     return (
         <div className="container">
+            {/* <button onClick={handlePrevClick} className="new-button prev"><IoIosArrowForward/></button>
+            <button onClick={handleNextClick} className="new-button next"></button> */}
+
             <Slider {...settings}>
                 {imgs.map((item, idx) => (
                     <div key={idx}>
--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,4 +1,5 @@
 import { ImageCarousel } from "./ImageCarousel";
+import { AiFillStar } from 'react-icons/ai'
 
 export function StayPreview({ stay }) {
     return (
@@ -8,9 +9,9 @@ export function StayPreview({ stay }) {
 
             <div className="stay-info">
                 <div className="stay-loc">{stay.loc.city}, {stay.loc.country}</div>
-                <span className="stay-rate">{4.55}</span>
-                <div>{stay.name}</div>
-                <div className="stay-price">{stay.price} night</div>
+                <span className="stay-rate"> <AiFillStar className="rate-star"/><span className="rate-num">{4.55}</span></span>
+                <div className="stay-name">{stay.name}</div>
+                <div className="stay-price">$ {stay.price} <span className="night">night</span></div>
             </div>
         </article>
     )
