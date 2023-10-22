9106ba446dbcd06661bca7293111ccc900db648a
Tal Elias <talelias2@gmail.com>
2023-10-22 11:18:40 +0300

    added helper

---
+++ src/cmps/StayPreview_temp.jsx
@@ -0,0 +1,21 @@
+import { ImageCarousel } from "./ImageCarousel";
+import { ReviewRate } from "./stay-reviews/ReviewRate";
+
+export function StayPreview({ stay }) {
+    return (
+        <article className="stay-preview">
+            {/* <img className="stay-img" src={stay.imgUrls[0]} /> */}
+            <ImageCarousel imgs={stay.imgUrls} />
+
+            <div className="preview-info">
+                <div className="preview-header flex space-between">
+                    <div className="stay-loc">{stay.loc.city}, {stay.loc.country}</div>
+                    <ReviewRate reviews={stay.reviews} />
+                </div>
+                {/* <span className="stay-rate"> <AiFillStar className="rate-star"/><span className="rate-num">{4.55}</span></span> */}
+                <div className="stay-name">{stay.name}</div>
+                <div className="stay-price">$ {stay.price} <span className="night">night</span></div>
+            </div>
+        </article>
+    )
+}
