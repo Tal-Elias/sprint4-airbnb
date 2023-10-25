16fd60beaa4dfec5939fba3eb7dc255e00e37c46
Tal Elias <talelias2@gmail.com>
2023-10-25 13:59:35 +0300

    implemented nav-menu in the header.

--- src/cmps/stay-details/GuestCounter.jsx
+++ src/cmps/stay-details/GuestCounter.jsx
@@ -14,10 +14,10 @@ export function GuestCounter() {
     }
 
     return (
-        <div>
-            <button onClick={handleDecrement}>-</button>
+        <div className="guest-counter flex align-center space-between">
+            <button onClick={handleDecrement}><svg viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', height: '12px', width: '12px', fill: 'currentcolor' }}><path d="m.75 6.75h10.5v-1.5h-10.5z"></path></svg></button>
             <div>{count}</div>
-            <button onClick={handleIncrement}>+</button>
+            <button onClick={handleIncrement}><svg viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', height: '12px', width: '12px', fill: 'currentcolor' }}><path d="m6.75.75v4.5h4.5v1.5h-4.5v4.5h-1.5v-4.5h-4.5v-1.5h4.5v-4.5z"></path></svg></button>
         </div>
     )
 }
---
+++ src/cmps/stay-details/GuestSelectModal.jsx
@@ -0,0 +1,44 @@
+import { GuestCounter } from "./GuestCounter"
+
+export function GuestSelectModal({ setGuestSelectModalOpen }) {
+
+    const guestTypes = [
+        {
+            label: 'Adults',
+            subLabel: 'Ages 13 or above',
+            field: 'adults'
+        },
+        {
+            label: 'Children',
+            subLabel: 'Ages 2-12',
+            field: 'children'
+        },
+        {
+            label: 'Infants',
+            subLabel: 'Under 2',
+            field: 'infants'
+        },
+        {
+            label: 'Pets',
+            subLabel: <a href="">Bringing a Service Animal?</a>,
+            field: 'pets'
+        }
+    ]
+
+    return (
+        <div className="guest-select-modal">
+            {guestTypes.map((type, idx) => {
+                return (
+                    <div className="guest-select-row flex space-between" key={idx}>
+                        <div className="guest-select-labels">
+                            <div className="guest-select-label">{type.label}</div>
+                            <div className="guest-select-sub-label">{type.subLabel}</div>
+                        </div>
+                        <GuestCounter />
+                    </div>
+                )
+            })}
+            <button onClick={() => setGuestSelectModalOpen(false)}>Close</button>
+        </div>
+    )
+}
